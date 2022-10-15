8a8a0be242799520a94af105a039f5008d289029
Aashish-Kumar-saini <72143076+Aashish-Kumar-saini@users.noreply.github.com>
2022-10-15 21:57:17 +0530

    Create About.js

---
+++ todos/src/AllComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h4>About Us</h4>
+            <p>We recently created <mark>Todos-list</mark> so that 
+            users can add there Todos in it and can complete 
+            there task on time .</p>
+            <p>For know more about us you can click on <strong><i>contact us</i></strong> on the nav bar</p>
+        </div>
+    )
+}
